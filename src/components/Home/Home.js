e1fba860cdfd92f49c199bb95376a6980f0a0626
unknown <Supravat Sarkar>
2022-03-29 17:43:39 +0530

    [add] components

---
+++ src/components/Home/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Banner from '../Banner/Banner';
+import Services from '../Services/Services';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import Contact from '../Contact/Contact';
+import About from '../About/About';
+
+const Home = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Banner></Banner>
+            <Services></Services>
+            <About></About>
+            <Contact></Contact>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Home;
